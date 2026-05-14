68882f2669cc8ed3132461e8775f2988858d93c6
Michael Yaacoub <csdmichael@gmail.com>
2026-05-14 10:33:05 -0700

    api deploy fix

--- api/startup.sh
+++ api/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 cd /home/site/wwwroot
-pip install --quiet --disable-pip-version-check -r requirements.txt
-python -m uvicorn server:app --host 0.0.0.0 --port 8000
+
+# Use pre-installed packages from CI build (skip slow runtime pip install)
+export PYTHONPATH="/home/site/wwwroot/.python_packages/lib/site-packages:${PYTHONPATH:-}"
+
+python -m uvicorn server:app --host 0.0.0.0 --port "${PORT:-8000}"
